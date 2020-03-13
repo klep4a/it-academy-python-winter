431df845f57e2480b9e8a3197691e3671485edab
klep4a <klep4a@users.noreply.github.com>
2020-03-13 18:07:12 +0300

    fix after review, rewrite decorator

--- src/Homework6/parametrized_decorator.py
+++ src/Homework6/parametrized_decorator.py
@@ -5,30 +5,38 @@
 """
 
 
-def count_errors(errors):
-    count = 0
-
-    def decorator(f):
+def count_errors(max_errors, runs):
+    def decorator(func):
         def wrapper(*args, **kw):
-            nonlocal count
-            try:
-                return f(*args, **kw)
-            except Exception as e:
-                count += 1
-                while count < errors:
-                    return e.args[0]
-                print('TooManyErrors')
-                raise
+            calls = 0
+            errors = 0
+
+            class TooManyErrors(Exception):
+                pass
+
+            for _ in range(runs):
+                calls += 1
+                try:
+                    res = func(*args, **kw)
+                    print((f'Call {calls} to "{func.__name__}",'
+                           f' Errors: {errors}, Result: {res}'))
+                except Exception as e:
+                    errors += 1
+                    if errors > max_errors:
+                        res = e.args[0]
+                        raise TooManyErrors(f'Call {calls} to "{func.__name__}",'
+                                            f' Errors: {errors}, Result: {res}')
+                    res = e.args[0]
+                    print((f'Call {calls} to "{func.__name__}",'
+                           f' Errors: {errors}, Result: {res}'))
         return wrapper
     return decorator
 
 
-@count_errors(3)
+@count_errors(max_errors=2, runs=4)
 def div(a, b):
     return a / b
 
 
-print(div(1, 2))
-print(div(1, 0))
-print(div(1, '2'))
-print(div(1, None))
+div(1, 2)
+div(1, None)
